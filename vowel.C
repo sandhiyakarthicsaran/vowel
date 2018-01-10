4386e4ff3ad87c1cae3986859c966f57891a6f6a
sandhiyakarthicsaran <35221190+sandhiyakarthicsaran@users.noreply.github.com>
2018-01-10 16:58:45 +0530

    vowel.C
    vowel.c

---
+++ vowel.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("Enter the character:");
+scanf("%c",&ch);
+if(ch=='a'//ch=='A'//ch=='e'//ch=='E'//ch=='i'//ch=='I'//ch=='o'//ch=='O'//ch=='u'//ch=='U')
+printf("%c is vowel");
+else
+printf("%c is consonent");
+return 0;
